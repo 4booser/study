05a96e34c92e707eb9f0d83b7a15d9c40305d2eb
Towikk <anton.masuytkin@gmail.com>
2026-01-08 00:19:59 +0200

    hello world

---
+++ WebApplication1/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("new_list")]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return Ok("API работает");
+        }
+
+        [HttpPost("post")]
+        public IActionResult Post(string _list)
+        {
+            Console.WriteLine(_list);
+            return Ok("API работает");
+        }
+    }
+}
+
+
+
+
+ 
+
